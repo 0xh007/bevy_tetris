f639c68defc4beb0c29a0518fa04c99388b3af72
Victor Rogers <victor.r.rogers@protonmail.com>
2020-09-11 08:00:07 -0600

    more work to fixing first camera

--- src/systems/render.rs
+++ src/systems/render.rs
@@ -3,14 +3,14 @@ use bevy::prelude::*;
 pub fn render_setup(
     mut commands: Commands,
 ) {
-    commands.spawn(
-        Camera3dComponents {
-            transform: Transform::new_sync_disabled(Mat4::face_toward(
-                Vec3::new(0.0, 0.0, 0.0),
-                Vec3::new(0.0, 0.0, 0.0),
-                Vec3::new(0.0, 0.0, 0.0),
-             )),
-             ..Default::default()
-        });
+    commands.spawn(Camera3dComponents {
+       transform: Transform::new_sync_disabled(Mat4::face_toward(
+           Vec3::new(0.0, 0.0, 0.0),
+           Vec3::new(0.0, 0.0, 0.0),
+           Vec3::new(0.0, 0.0, 0.0),
+       )),
+       ..Default::default()
+    });
+
     println!("Render setup complete");
 }
