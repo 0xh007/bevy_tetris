290b54a7ed16396160d0b9afaec4828b3032a36b
Victor Rogers <victor.r.rogers@protonmail.com>
2020-09-27 16:21:06 -0600

    removed code for the old fall system

--- src/systems/fall.rs
+++ src/systems/fall.rs
@@ -26,7 +26,6 @@ pub fn fall_system(
             let block_translation = Vec3::new(x, y, z);
 
             let cell = tetris_grid.update_position(block_translation, block.last_grid_pos, block.state);
-            // delete this comment
 
             if cell != block.current_grid_pos {
                 block.last_grid_pos = block.current_grid_pos;
@@ -44,43 +43,6 @@ pub fn fall_system(
         }
     }
 }
-
-// TODO: Remove once new system is fully in place
-/*
-pub fn fall_system_old(
-    time: Res<Time>,
-    mut testris_grid: ResMut<TetrisGrid>,
-    mut tetronimo_query: Query<(&mut TetronimoBlock, &GlobalTransform, &mut Transform)>,
-) {
-    for (mut tetronimo, global_transform, mut transform) in &mut tetronimo_query.iter() {
-        let direction = -1.0;
-        let global_translation = global_transform.translation();
-        let mut translation = transform.translation();
-
-        let position = Vec3::new(global_translation.x(), global_translation.y(), global_translation.z());
-        let cell = testris_grid.update_position(position, tetronimo.last_grid_pos, tetronimo.state);
-
-        if cell != tetronimo.current_grid_pos {
-            tetronimo.last_grid_pos = tetronimo.current_grid_pos;
-            tetronimo.current_grid_pos = cell;
-        }
-
-        println!("-----");
-        println!("{}", tetronimo.name);
-        println!("{}", tetronimo.state);
-        println!("Physical Pos ({}, {})", global_translation.x(), global_translation.y());
-        println!("Last Grid Pos [{}][{}]", tetronimo.last_grid_pos.0, tetronimo.last_grid_pos.1);
-        println!("Current Grid Pos [{}][{}]", tetronimo.current_grid_pos.0, tetronimo.current_grid_pos.1);
-        println!("-----");
-        
-        if !TetrisGrid::is_cell_below_occupied(&testris_grid, cell.0, cell.1) {
-            *translation.y_mut() += time.delta_seconds * direction * tetronimo.speed;
-        } else {
-            tetronimo.state = TetronimoState::Stopped;
-        }
-    }
-}
-*/
 
 pub fn grid_debug_system(
     mut testris_grid: ResMut<TetrisGrid>,
