734c0d8a2470eb579e036483d598271c11d48702
Victor Rogers <victor.r.rogers@protonmail.com>
2020-09-28 08:28:37 -0600

    initial work to horizontal movement system

---
+++ src/systems/horizontal_movement.rs
@@ -0,0 +1,12 @@
+use bevy::prelude::*;
+use crate::TetrisGrid;
+use crate::TetronimoBlock;
+use crate::TetronimoState;
+
+pub fn horizontal_movement_system(
+    time: Res<Time>,
+    mut tetris_grid: ResMut<TetrisGrid>,
+    mut tetronimo_query: Query<(&mut Tetronimo, &mut Transform, &mut Children)>,
+    block_query: Query<(&mut TetronimoBlock, &Transform)>,
+) {
+}
